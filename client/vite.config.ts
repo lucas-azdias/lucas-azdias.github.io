295d11f0885dd03917644c39aa020beb9ba6b67a
Lucas Dias <lucas.azevedodias@outlook.com>
2026-01-30 14:11:56 -0300

    fix: redirecting on direct url access

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 import { type UserConfig } from "vite";
 
 export default {
-    base: "./",
+    base: "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
