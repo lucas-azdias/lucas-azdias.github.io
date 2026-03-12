4c2671bf68943b74df3fbd2e5cd024ec43c34cb5
Lucas Dias <lucas.azevedodias@outlook.com>
2026-03-12 20:07:46 -0300

    feat: solutions renamed to projects

--- client/src/constants/pages.ts
+++ client/src/constants/pages.ts
@@ -6,7 +6,6 @@ export interface Page {
 // Central list of all pages
 export const PAGES: Page[] = [
     { label: "SOBRE", to: "/capitulos/sobre" },
-    { label: "SOLUÇÕES", to: "/capitulos/solucoes" },
     { label: "PROJETOS", to: "/capitulos/projetos" },
     { label: "ARTIGOS", to: "/capitulos/artigos" },
     { label: "ARQUIVOS", to: "/capitulos/arquivos" },
